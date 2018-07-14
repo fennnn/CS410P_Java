5a06ee477fa2f06f58dd0ee853458c2e33fe8be7
fennnn <32502568+fennnn@users.noreply.github.com>
2018-07-14 16:08:35 -0700

    Add new phonebill class

--- phonebill/src/main/java/edu/pdx/cs410J/fen/PhoneCall.java
+++ phonebill/src/main/java/edu/pdx/cs410J/fen/PhoneCall.java
@@ -7,55 +7,53 @@ import java.util.Date;
 
 public class PhoneCall extends AbstractPhoneCall {
 
-  private String Caller;
-  private String Callee;
-  private String startTime;
-  private String endTime;
-  private String startDate;
-  private String endDate;
+    private String Caller;
+    private String Callee;
+    private String startTime;
+    private String endTime;
+    private String startDate;
+    private String endDate;
 
-  PhoneCall()
-  {
+    PhoneCall() {
 
-  }
+    }
 
-  PhoneCall(String caller, String callee, String startDate, String startTime, String endDate, String endTime)
-  {
-    super();
+    PhoneCall(String caller, String callee, String startDate, String startTime, String endDate, String endTime) {
+        super();
 
-    this.Caller = caller;
-    this.Callee = callee;
-    this.startDate = startDate;
-    this.startTime = startTime;
-    this.endDate = endDate;
-    this.endTime = endTime;
-  }
+        this.Caller = caller;
+        this.Callee = callee;
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.endDate = endDate;
+        this.endTime = endTime;
+    }
 
-  @Override
-  public String getCaller() {
-    if (this.Callee.isEmpty())
-      return "This method is not implemented yet";
-    else
-      return  Caller;
-  }
+    @Override
+    public String getCaller() {
+        if (this.Callee.isEmpty())
+            return "This method is not implemented yet";
+        else
+            return Caller;
+    }
 
 
-  @Override
-  public String getCallee() {
-    if (this.Callee.isEmpty())
-      return "This method is not implemented yet";
-    else
-      return Callee;
-  }
+    @Override
+    public String getCallee() {
+        if (this.Callee.isEmpty())
+            return "This method is not implemented yet";
+        else
+            return Callee;
+    }
 
-  @Override
-  public String getStartTimeString() {
+    @Override
+    public String getStartTimeString() {
 
-    return startDate+" "+startTime;
-  }
+        return startDate + " " + startTime;
+    }
 
-  @Override
-  public String getEndTimeString() {
-      return endDate+" "+endTime;
-  }
+    @Override
+    public String getEndTimeString() {
+        return endDate + " " + endTime;
+    }
 }
